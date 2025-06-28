1ee25febb45037243eb8e53562350d041edb65d3
Simrit <kaulsimrit@gmail.com>
2025-06-28 13:24:39 +0530

    Get weather

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,31 @@ const API_WEATHER_BASE_URL = process.env.API_WEATHER_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-const cityName = "Liverpool";
+router.get("/weather", async (req, res) => {
+  const { cityName } = req.query;
+  if (!cityName) {
+    return res.status(400).json({ error: "City name is required" });
+  }
 
-router.get("/", async (req, res) => {
   try {
     const geoRes = await needle(
       "get",
       `${API_GEO_BASE_URL}?q=${cityName}&limit=5&${API_KEY_NAME}=${API_KEY_VALUE}`
     );
     const geoData = geoRes.body;
-    res.status(200).json(geoData);
+
+    if (geoData.length === 0) {
+      return res.status(404).json({ error: "City not found" });
+    }
+
+    const { lat, lon } = geoData[0];
+    const weatherRes = await needle(
+      "get",
+      `${API_WEATHER_BASE_URL}?lat=${lat}&lon=${lon}&${API_KEY_NAME}=${API_KEY_VALUE}`
+    );
+    const weatherData = weatherRes.body;
+
+    res.status(200).json(weatherData);
   } catch (error) {
     res.status(500).json({ error });
   }
